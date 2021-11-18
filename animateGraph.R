8316846004191c5aa9d62495068771976561c3eb
Ruby Adkins <radkins22@students.stab.org>
2021-11-18 11:51:16 -0500

    final touches

--- animateGraph.R
+++ animateGraph.R
@@ -2,9 +2,11 @@ library(shiny)
 library(ggplot2)
 library(gapminder)
 library(dplyr)
+library(rsconnect)
+library(ggpmisc)
 
 ui <- fluidPage(
-  titlePanel(title=h4("graph", align="center")),
+  titlePanel(title=h4("Life Expectancy versus GDP per Capita", align="center")),
   sidebarPanel( 
     sliderInput("yearValue", "Year:",min = 1952, max = 2002,step=5,value=1952, sep = "", animate=TRUE)),
   mainPanel(plotOutput("plot")))
@@ -25,8 +27,10 @@ server <- function(input,output){
                    parse = TRUE, label.y = "bottom", label.x = "right") + 
       scale_size(range = 2) +
       scale_x_log10() + 
-      facet_wrap(~continent)
-    },height = 400,width = 600)
+      facet_wrap(~continent) +
+      xlab("GDP per Capita") +
+      ylab("Life Expectancy") 
+    }, height = 400,width = 600)
   }
 
 shinyApp(ui, server)

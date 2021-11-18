84328e3014881f813e4b32a5cddccf8f8c70a899
Ruby Adkins <radkins22@students.stab.org>
2021-11-18 10:38:27 -0500

    add multiple country groups

--- graph.R
+++ graph.R
@@ -2,12 +2,11 @@ library(ggplot2)
 library(gapminder)
 library(dplyr)
 
-
 gapminder %>%
   filter(year == 1952) %>%
-  ggplot(gapminder, mapping = aes(gdpPercap, lifeExp, size = pop, colour = country)) +
+  ggplot(gapminder, mapping = aes(x = gdpPercap, y = lifeExp, size = pop, colour = continent)) +
   geom_point(alpha = 0.7, show.legend = FALSE) +
-  scale_colour_manual(values = country_colors) +
+  geom_smooth(method="lm",se = FALSE) +
   scale_size(range = c(2, 12)) +
-  scale_x_log10() +
+  scale_x_log10() + 
   facet_wrap(~continent)
